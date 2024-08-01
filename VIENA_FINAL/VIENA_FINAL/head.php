eba7379e1574830394821c7ef04a14f86c021c17
Rubí <ramseymegan17@gmail.com>
2024-08-01 13:22:02 -0500

    Update head.php

--- VIENA_FINAL/VIENA_FINAL/head.php
+++ VIENA_FINAL/VIENA_FINAL/head.php
@@ -31,10 +31,8 @@
 <header id="header" class="fixed-top-custom">
     <nav class="navbar navbar-expand-lg navbar-dark">
         <div class="container-fluid"> <!-- Cambiado de container a container-fluid -->
-            <a class="navbar-brand" href="iniciar.php">VIENA INGENIEROS<span>.</span></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            <p class="navbar-brand" href="index.php">VIENA INGENIEROS<span>.</span></p>
+           
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
