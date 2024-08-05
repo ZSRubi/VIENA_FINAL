e89d81025886ad192d696d1c7106b592c0269a12
Rubí <ramseymegan17@gmail.com>
2024-08-05 08:48:55 -0500

    Update index.php

--- VIENA_FINAL/VIENA_FINAL/index.php
+++ VIENA_FINAL/VIENA_FINAL/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-sacalable=no, initial-scale=1.0, maximun-scale =1.0,minimun-scale=1.0">
     <link rel="stylesheet" href="estilos/index.css">
     <title>VIENA INGENIEROS</title>
 
